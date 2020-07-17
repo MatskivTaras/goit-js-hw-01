115596a1d761d696c3597c0b46b06f2cfa31f833
Matskiv Taras <taras53421@ukr.net>
2020-07-17 14:02:24 +0300

    add tasks 1, 2

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+console.log("task-2 {");
+
+const total = 100;
+const ordered = 120;
+
+if (total < ordered) {
+  console.log("На складі недостатньо товарів!");
+} else {
+  console.log("Замовлення оформлено, з вами зв'яжеться менеджер");
+}
+
+console.log("} task-2");
