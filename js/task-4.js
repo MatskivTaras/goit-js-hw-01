628282e4a9062f8a73ebd67d830bf78a70a5b461
Matskiv Taras <taras53421@ukr.net>
2020-07-20 12:18:56 +0300

    change log string to templates row in tasks 1,4

--- js/task-4.js
+++ js/task-4.js
@@ -8,21 +8,14 @@ if ( isNaN(payAmount) ) {
   if (payAmount === null) {
     console.log("Скасовано користувачем!");
   } else {
-    const credits = 23580,
-      pricePerDroid = 3000,
-      totalPrice = payAmount * pricePerDroid;
+    const credits = 23580;
+    const pricePerDroid = 3000;
+    const totalPrice = payAmount * pricePerDroid;
 
     if (totalPrice > credits) {
       console.log("Недостатньо коштів на рахунку!");
     } else {
-      const creditsRest = credits - totalPrice;
-      console.log(
-        "Ви купили " +
-          payAmount +
-          " дроїдів(-а), на рахунку залишилося " +
-          creditsRest +
-          " кредитів(-а)."
-      );
+      console.log(`Ви купили ${payAmount} дроїдів(-а), на рахунку залишилося ${credits - totalPrice} кредитів(-а).`);
     }
   }
 }
